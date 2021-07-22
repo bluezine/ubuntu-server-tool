1f210adb6571587ed5d2ef29f0f76c7b88099214
Kisig Ian Seo <bluezine0930@gmail.com>
2021-07-22 14:11:46 +0900

    무중단 서비스 실행

---
+++ shell script/active-service.sh
@@ -0,0 +1,13 @@
+service=$1
+status=$(systemctl status $service | awk '/Active/{print $2}')
+  
+if [ "$status" ];
+then
+        if [ "$status" == "active" ];
+        then
+                echo "Already"
+        else
+                /usr/bin/systemctl restart $service
+        fi
+fi
+exit 0
